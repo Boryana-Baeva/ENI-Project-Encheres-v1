b67d054a497069f863f585dea56e23192154a7ea
Portable <Portable@LAPTOP-S6SK4PA4.home>
2021-01-04 23:08:24 +0100

    Test

--- ENI-Encheres/src/fr/eni/encheres/bo/Categorie.java
+++ ENI-Encheres/src/fr/eni/encheres/bo/Categorie.java
@@ -1,5 +1,7 @@
 package fr.eni.encheres.bo;
 
-public class Categorie {
+import java.io.Serializable;
 
+public class Categorie implements Serializable {
+private static final long serialVersionUID= 1L;
 }
