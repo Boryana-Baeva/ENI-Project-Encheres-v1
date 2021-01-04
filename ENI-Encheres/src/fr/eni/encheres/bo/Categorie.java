0364064ea3447784e47b24d7ce970ff4d07b87e6
Portable <Portable@LAPTOP-S6SK4PA4.home>
2021-01-04 23:15:55 +0100

    Categories

--- ENI-Encheres/src/fr/eni/encheres/bo/Categorie.java
+++ ENI-Encheres/src/fr/eni/encheres/bo/Categorie.java
@@ -3,5 +3,72 @@ package fr.eni.encheres.bo;
 import java.io.Serializable;
 
 public class Categorie implements Serializable {
-private static final long serialVersionUID= 1L;
+	private static final long serialVersionUID= 1L;
+	private int id;
+	private String libelle;
+	
+	
+	//Constructor 
+	
+	/**
+	 * @param id
+	 * @param libelle
+	 */
+	public Categorie(int id, String libelle) {
+		this.id = id;
+		this.libelle = libelle;
+	}
+	
+	/**
+	 * 
+	 */
+	public Categorie() {
+	}
+	
+
+	/**
+	 * @param libelle
+	 */
+	public Categorie(String libelle) {
+		this.libelle = libelle;
+	}
+	
+
+	// Getters And Setters 
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	
+	
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the libelle
+	 */
+	public String getLibelle() {
+		return libelle;
+	}
+	/**
+	 * @param libelle the libelle to set
+	 */
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	
+	// toString
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Categorie [id=" + id + ", libelle=" + libelle + "]";
+	}
 }
