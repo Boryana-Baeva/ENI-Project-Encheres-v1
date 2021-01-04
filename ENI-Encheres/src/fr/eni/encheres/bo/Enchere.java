4b2126c68c78afbb80420bc601ad89a15e5f794d
bbaeva2020 <bbaeva2020@DEV-1-EEL-485.ad.campus-eni.fr>
2021-01-04 23:39:40 +0100

    Enchere création

--- ENI-Encheres/src/fr/eni/encheres/bo/Enchere.java
+++ ENI-Encheres/src/fr/eni/encheres/bo/Enchere.java
@@ -1,7 +1,81 @@
 package fr.eni.encheres.bo;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 public class Enchere implements Serializable{
 	private static final long serialVersionUID = 1L;
+	private int id;
+	private LocalDate date;
+	private int montant;
+	private ArticleVendu article;
+	private Utilisateur encherisseur;
+	
+	// Constructeurs
+	
+	public Enchere() {
+		
+	}
+	
+	public Enchere(LocalDate date, int montant, ArticleVendu article, Utilisateur encherisseur) {
+		this.date = date;
+		this.montant = montant;
+		this.article = article;
+		this.encherisseur = encherisseur;
+	}
+
+	public Enchere(int id, LocalDate date, int montant, ArticleVendu article, Utilisateur encherisseur) {
+		this(date, montant, article, encherisseur);
+		this.id = id;	
+	}
+
+	// Gettes & Setters
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public LocalDate getDate() {
+		return date;
+	}
+	
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	public int getMontant() {
+		return montant;
+	}
+	
+	public void setMontant(int montant) {
+		this.montant = montant;
+	}
+	
+	public ArticleVendu getArticle() {
+		return article;
+	}
+	
+	public void setArticle(ArticleVendu article) {
+		this.article = article;
+	}
+	
+	public Utilisateur getEncherisseur() {
+		return encherisseur;
+	}
+	
+	public void setEncherisseur(Utilisateur encherisseur) {
+		this.encherisseur = encherisseur;
+	}
+
+	// ToString
+	@Override
+	public String toString() {
+		return "Enchere [id=" + id + ", date=" + date + ", montant=" + montant + ", article=" + article
+				+ ", encherisseur=" + encherisseur + "]";
+	}
+	
 }
