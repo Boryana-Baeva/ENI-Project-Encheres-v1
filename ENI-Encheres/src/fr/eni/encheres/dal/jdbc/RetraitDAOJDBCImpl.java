42617296f37979e0e319a89e27884b4271027005
Portable <Portable@LAPTOP-S6SK4PA4.home>
2021-01-05 23:59:57 +0100

    bougé de dossier

--- ENI-Encheres/src/fr/eni/encheres/dal/jdbc/RetraitDAOJDBCImpl.java
+++ ENI-Encheres/src/fr/eni/encheres/dal/jdbc/RetraitDAOJDBCImpl.java
@@ -1,4 +1,4 @@
-package fr.eni.encheres.dal;
+package fr.eni.encheres.dal.jdbc;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import fr.eni.encheres.bo.Retrait;
+import fr.eni.encheres.dal.RetraitDAO;
 
 
 
