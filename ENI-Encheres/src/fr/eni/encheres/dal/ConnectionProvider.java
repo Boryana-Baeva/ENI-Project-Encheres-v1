61966025fa739ec338dcff34055ad9add18efbcb
sarah <sarah@localhost>
2021-01-05 17:02:38 +0100

    maj

--- ENI-Encheres/src/fr/eni/encheres/dal/ConnectionProvider.java
+++ ENI-Encheres/src/fr/eni/encheres/dal/ConnectionProvider.java
@@ -8,35 +8,32 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
-
 public abstract class ConnectionProvider {
-	
-private static DataSource dataSource;
 
+	private static DataSource dataSource;
 
 	/**
 	 * Au chargement de la classe, la DataSource est recherchée dans l'arbre JNDI
 	 */
-	static
-	{
+	static {
 		Context contex;
 		try {
 			contex = new InitialContext();
-			ConnectionProvider.dataSource = (DataSource)contex.lookup("jdbc/pool_cnx");
+			ConnectionProvider.dataSource = (DataSource) contex.lookup("jdbc/pool_cnx");
 		} catch (NamingException e) {
 			e.printStackTrace();
 			throw new RuntimeException("Impossible d'accéder à la base de données");
 		}
 	}
-	
+
 	/**
-	 * Cette méthode retourne une connexion opérationnelle issue du pool de connexion
-	 * vers la base de donnee
+	 * Cette méthode retourne une connexion opérationnelle issue du pool de
+	 * connexion vers la base de donnee
+	 * 
 	 * @return
 	 * @throws SQLException
 	 */
-	public static Connection getConnection() throws SQLException 
-	{
+	public static Connection getConnection() throws SQLException {
 		return ConnectionProvider.dataSource.getConnection();
 	}
 
