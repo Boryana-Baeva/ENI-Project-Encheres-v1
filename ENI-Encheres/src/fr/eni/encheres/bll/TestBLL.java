0bae80dd42ceb84c2a5f2fa40a23f917a9971a11
sarah <sarah@localhost>
2021-01-07 15:24:57 +0100

    test bll et test dal

---
+++ ENI-Encheres/src/fr/eni/encheres/bll/TestBLL.java
@@ -0,0 +1,31 @@
+package fr.eni.encheres.bll;
+
+public class TestBLL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		/**
+		 * TEST UTILISATEUR
+		 */
+		UtilisateurManager.
+		
+		/**
+		 * TEST ARTICLE VENDU 
+		 */
+		
+		/**
+		 * TEST 
+		 */
+		
+		/**
+		 * TEST UTILISATEUR
+		 */
+		
+		/**
+		 * TEST UTILISATEUR
+		 */
+	}
+
+}
